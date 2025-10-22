e0f4db98991c7c826badda998e728b58342b7f46
towhid7667 <towhidulhq@gmail.com>
2025-10-23 02:40:02 +0600

    samesite none

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -27,7 +27,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
